2fc6f34c62ffb33155a44e238b6f787ceebddc7d
dario.zubaray <dzubaray@imagosur.com>
2019-10-21 08:49:32 -0300

    ciclo de vide del service worker

--- js/app.js
+++ js/app.js
@@ -6,6 +6,22 @@
 if( navigator.serviceWorker) {
     console.log('podemos utilizar SW');
 
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js').then( reg => {
+        setTimeout( () => {
+            reg.sync.register('posteo-gatitos');
+            console.log('Se enviaron fotos de gatitos al server');
+        }, 1000);
+
+        Notification.requestPermission().then(result => {
+            console.log('resultado de permiso de notificacion');
+            reg.showNotification('Hola mundo!');
+        });
+    });
+
 }
 
+if ( window.SyncManager ) {
+
+}
+
+fetch('https://reqres.in/api/users').then(resp => resp.text()).then(console.log);
--- sw.js
+++ sw.js
@@ -1,11 +1,40 @@
 
+
+self.addEventListener('install', evento => {
+    // Descargar Assets
+    // Crear Cache
+    console.log('Instalando SW');
+
+    const instalacion = new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            console.log('Instalaciones terminadas');
+            self.skipWaiting();
+            resolve();
+        }, 1500);
+    });
+    evento.waitUntil(instalacion);
+});
+
+
+self.addEventListener('activate', evento => {
+    //Borrar cache viejo
+    console.log('SW activo');
+});
+
+
 self.addEventListener('fetch', evento => {
+    // Aplicar estrategias del cache
+    console.log('SW fetch: ', evento.request.url);
+    if (evento.request.url.includes('https://reqres.in')) {
+        const respuesta = new Response(`{ok: false, mensaje: 'jajaja'}`);
+        evento.respondWith(respuesta);
+    }
     
     if (evento.request.url.includes('style.css')) {
         let respuesta = new Response(`
             body {
                 background-color: red;
-                color:pink;
+                color pink;
             }
         `, {headers: {'Content-Type': 'text/css'}});
         evento.respondWith(respuesta);
@@ -14,4 +43,17 @@ self.addEventListener('fetch', evento => {
     if (evento.request.url.includes('.jpg')) {
         evento.respondWith( fetch('img/main-patas-arriba.jpg'));
     }
+});
+
+
+// Recuperamos la conexion a internet
+self.addEventListener('sync', evento => {
+    console.log('Tenemos conexion!');
+    console.log('evento.tag', evento.tag);
+});
+
+
+// Manejar notificaciones push
+self.addEventListener('push', evento => {
+    console.log('Notificacion recibida!');
 });
